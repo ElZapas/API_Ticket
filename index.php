d1cbc2c1b3e1d812b21eea33a8b55ab55a34f0d0
Anstro <anstrosolano@gmail.com>
2024-10-30 19:25:36 -0500

    habilitando content type en los headers

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
 
 // Requiere los archivos de conexión a la base de datos y controladores de autenticación
 require 'db.php';
